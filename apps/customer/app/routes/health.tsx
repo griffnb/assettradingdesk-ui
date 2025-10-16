908544d864df9c0ad8feccf0a4f88faa233b8006
griffnb <griffnb@gmail.com>
2025-10-16 16:14:31 -0700

    test this
    Signed-off-by: griffnb <griffnb@gmail.com>

--- apps/customer/app/routes/health.tsx
+++ apps/customer/app/routes/health.tsx
@@ -1,4 +1,13 @@
-export default function Page() {
-  console.log(import.meta.env);
+import { Route } from "../+types/root";
+
+export async function loader() {
+  return {
+    processEnv: process.env,
+    metaEnv: import.meta.env,
+  };
+}
+export default function Page({ loaderData }: Route.ComponentProps) {
+  console.log("Client", import.meta.env);
+  console.log("Server", loaderData);
   return <>1</>;
 }
