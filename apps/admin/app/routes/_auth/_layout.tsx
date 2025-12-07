94b9e5a872c30fbd524b228d803b97f05c520a8c
griffnb <griffnb@gmail.com>
2025-12-07 16:53:50 -0700

    cleanup auth
    Signed-off-by: griffnb <griffnb@gmail.com>

--- apps/admin/app/routes/_auth/_layout.tsx
+++ apps/admin/app/routes/_auth/_layout.tsx
@@ -1,7 +1,13 @@
+import { useAdmin } from "@/common_lib/authentication/useAdmin";
 import { AdminLayout } from "@/ui/admin/layout/AdminLayout";
 import { Outlet } from "react-router";
 
 export default function RootIndex() {
+  const { admin, adminLoading } = useAdmin();
+  if (adminLoading || !admin) {
+    return null;
+  }
+
   return (
     <AdminLayout>
       <Outlet />
--- packages/common_lib/src/authentication/useAccount.ts
+++ packages/common_lib/src/authentication/useAccount.ts
@@ -1,13 +1,26 @@
 import { SessionService } from "@/common_lib/services/SessionService";
 
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
 
-export const useAccount = (force?: boolean) => {
+interface UseAccountOptions {
+  force?: boolean;
+  redirectLocation?: string;
+}
+
+export const useAccount = ({
+  force,
+  redirectLocation,
+}: UseAccountOptions = {}) => {
   const [accountLoading, setAccountLoading] = useState(true); // Indicates if the auth check is in progress
+  const nav = useNavigate();
   useEffect(() => {
     SessionService.loadAccount(force).then((account) => {
       if (!account) {
         setAccountLoading(false); // Auth check is complete
+        if (redirectLocation) {
+          nav(redirectLocation);
+        }
         return;
       }
 
